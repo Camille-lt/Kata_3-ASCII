683091da9ef0286d1dbacef9eefb22a4241fb688
Camille-lt <camillelebigot5@gmail.com>
2025-05-09 11:15:44 +0200

    add drawing ascii

---
+++ script.js
@@ -0,0 +1,39 @@
+const calendrierDuCrayon = {
+    septembre: 10,
+    octobre: 9,
+    novembre: 8,
+    decembre: 7,
+    janvier: 6,
+    fevrier: 5,
+    mars: 4,
+    avril: 3,
+    mai: 2,
+    juin: 1,
+};
+
+function drawMyPen (mois) {
+const nbrEtages = calendrierDuCrayon[mois];
+let pen = '';
+
+pen += ' /\\\n/__\\\n';
+for (let i = 0; i < nbrEtages; i++) {
+    pen += '||||\n';  // Ajouter un étage à chaque itération
+}
+if (mois === 'mars') {
+    pen += '|__|\n';
+} else if ( mois === 'avril') {
+    pen += '|__|\n';
+} else if (mois === 'mai') {
+    pen += '|__|\n';
+} else if (mois === 'juin') {
+    pen += '|__|\n';
+} else {
+    pen += '|__|\n|  |\n|__|\n'; 
+}
+return pen
+}
+console.log(drawMyPen('septembre'));
+console.log(drawMyPen('fevrier'));
+console.log(drawMyPen('mars'));
+console.log(drawMyPen('juin'));
+console.log(drawMyPen('octobre')); // pour exemple
